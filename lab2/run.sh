99c1888c5705d45d92adf5ea088eba7444cbb220
anras5 <anras1filip@gmail.com>
2024-10-30 22:03:24 +0100

    feat: add run for lab2

---
+++ lab2/run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+export DIR_WITH_FRAMS_LIBRARY=/Users/filipmarciniak/studia/st2sem2/amib/Framsticks50
+cd ../framspy
+
+# Oznaczenia genotypów
+G_values=(0 1 4 9)
+
+# funkcja uruchamiająca algorytm genetyczny dla danego genotypu
+run_algorithm() {
+    iteration="$1"
+    G="$2"
+
+    start_time=$(date +%s)
+
+    # Uruchomienie algorytmu
+    uv run FramsticksEvolution.py \
+        -path "$DIR_WITH_FRAMS_LIBRARY" \
+        -sim "eval-allcriteria.sim;deterministic.sim;sample-period-2.sim;only-body.sim" \
+        -opt vertpos -max_numparts 30 -max_numgenochars 50 -genformat "$G" \
+        -popsize 60 -generations 150 -hof_size 1 \
+        -hof_savefile "lab2/f$G/HoF/$iteration.gen" \
+        -deap_logfile "lab2/f$G/Deap/$iteration.csv"
+
+    # Wypisanie czasu wykonania algorytmu do times.txt
+    end_time=$(date +%s)
+    elapsed_time=$((end_time - start_time))
+    echo "${iteration},${elapsed_time}" >> "lab2/f$G/times.txt"
+
+}
+
+export -f run_algorithm
+
+for G in "${G_values[@]}"; do
+    mkdir -p "lab2/f$G/HoF/"
+    mkdir -p "lab2/f$G/Deap/"
+    rm -f "lab2/f$G/times.txt"
+    echo "N,time" >> "lab2/f$G/times.txt"
+    # Uruchomienie algorytmu dla każdej wartości G przy użyciu parallel
+    iterations=(1 2 3 4 5 6 7 8 9 10)
+    parallel run_algorithm ::: "${iterations[@]}" ::: "$G"
+
+    echo "----------------------------------------------------------------------"
+    echo "Finished for f$G"
+done

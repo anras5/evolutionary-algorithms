ac66af3685972b1747a74255ae6618a635556c65
anras5 <anras1filip@gmail.com>
2025-01-07 10:27:08 +0100

    feat: add additional versions of gp to run-gp.sh

--- lab6/run-gp.sh
+++ lab6/run-gp.sh
@@ -1,36 +1,55 @@
 #!/bin/bash
 
 export DIR_WITH_FRAMS_LIBRARY=/Users/filipmarciniak/studia/st2sem2/amib/Framsticks50
-cp -f FramsticksEvolution.py ../framspy/FramsticksEvolution.py
+cp -f FramsticksEvolutionGP.py ../framspy/FramsticksEvolutionGP.py
+cp -f FramsticksEvolutionGPBare.py ../framspy/FramsticksEvolutionGPBare.py
 cd ../framspy || exit
+
+GPs=("gp200" "gp500" "gb200" "gb500")
+
 
 
 run_algorithm() {
   iteration="$1"
+  directory="$2"
   start_time=$(date +%s)
 
-  uv run FramsticksEvolution.py \
+  # if directory starts with gp then run FramsticksEvolutionGP.py
+  # if directory starts with gb then run FramsticksEvolutionGPBare.py
+  if [[ $directory == gp* ]]; then
+    script="FramsticksEvolutionGP.py"
+  else
+    script="FramsticksEvolutionGPBare.py"
+  fi
+
+  uv run $script \
     -path "$DIR_WITH_FRAMS_LIBRARY" \
     -generations 150 \
-    -popsize 200 \
+    -popsize "$3" \
     -opt vertpos \
     -hof_size 1 \
-    -hof_savefile "lab6/gp/HoF/$iteration.gen" \
-    -deap_logfile "lab6/gp/Deap/$iteration.csv"
+    -hof_savefile "lab6/$directory/HoF/$iteration.gen" \
+    -deap_logfile "lab6/$directory/Deap/$iteration.csv"
 
   end_time=$(date +%s)
   elapsed_time=$((end_time - start_time))
-  echo "${iteration},${elapsed_time}" >> "lab6/gp/times.txt"
+  echo "${iteration},${elapsed_time}" >> "lab6/$directory/times.txt"
 }
 
 export -f run_algorithm
 
-mkdir -p "lab6/gp/HoF/"
-mkdir -p "lab6/gp/Deap/"
-rm -f "lab6/gp/times.txt"
-echo "N,time" >> "lab6/gp/times.txt"
-iterations=(1 2 3 4 5 6 7 8 9 10)
-parallel run_algorithm ::: "${iterations[@]}"
+# Run the algorithm for "gp200" and "gp500"
+for gp in "${GPs[@]}"; do
+  echo "----------------------------------------------------------------------"
+  echo "Running for $gp"
+  mkdir -p "lab6/$gp/HoF/"
+  mkdir -p "lab6/$gp/Deap/"
+  rm -f "lab6/$gp/times.txt"
+  echo "N,time" >> "lab6/$gp/times.txt"
+  iterations=(1 2 3 4 5 6 7 8 9 10)
+  parallel run_algorithm ::: "${iterations[@]}" ::: "$gp" ::: "${gp:2}"
+  echo "----------------------------------------------------------------------"
+  echo "Finished for $gp"
+done
 
-echo "----------------------------------------------------------------------"
-echo "Finished for gp"
+
